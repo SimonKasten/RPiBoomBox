100c9e7b4acc4702be316a6b3d6b7b58a6a92d39
SimonKasten <30275716+SimonKasten@users.noreply.github.com>
2017-07-19 03:01:05 +0200

    Add files via upload

---
+++ bt_pa_config.sh
@@ -0,0 +1,165 @@
+#!/bin/bash
+
+#The bluetooth device can appear as a number of things.
+#Change the line '+ Class = 0x200414' below to one of the following classes;
+# 0x200418 - Headphones
+# 0x200414 - Loudspeaker (default)
+# 0x200420 - Car Audio
+# 0x200434 - Car audio, Loudspeaker
+# 0x200438 - Car Audio, Headphones
+#
+# All of the above classes show the headphone icon on iOS when connected.
+# If you wish to not have that icon then use one of the following;
+# 0x20041C - Headphones, Portable Audio / Portable Audio
+# 0x20043C - Headphones, Portable Audio, Car audio / Portable Audio, Car audio
+
+
+read -p "Bluetooth device name: " BT_NAME
+
+#--------------------------------------------------------------------
+function tst {
+    echo "===> Executing: $*"
+    if ! $*; then
+        echo "Exiting script due to error from: $*"
+        exit 1
+    fi	
+}
+#--------------------------------------------------------------------
+
+
+
+mkdir /home/pi/pyScripts
+tst cp usr/local/bin/volume-watcher.py /usr/local/bin/volume-watcher.py
+tst chmod +x /usr/local/bin/volume-watcher.py
+tst cp lib/systemd/system/volume-watcher.service /lib/systemd/system/volume-watcher.service
+tst systemctl enable volume-watcher
+tst cd `dirname $0`
+
+echo "PRETTY_HOSTNAME=$BT_NAME" >> /tmp/machine-info
+tst sudo cp /tmp/machine-info /etc
+
+tst sudo cp init.d/pulseaudio /etc/init.d
+tst sudo chmod +x /etc/init.d/pulseaudio
+tst sudo update-rc.d pulseaudio defaults
+
+tst sudo cp init.d/bluetooth /etc/init.d
+tst sudo chmod +x /etc/init.d/bluetooth
+tst sudo update-rc.d bluetooth defaults
+
+tst sudo cp init.d/bluetooth-agent /etc/init.d
+tst sudo chmod +x /etc/init.d/bluetooth-agent
+tst sudo update-rc.d bluetooth-agent defaults
+
+tst sudo cp usr/local/bin/bluez-udev /usr/local/bin
+tst sudo chmod 755 /usr/local/bin/bluez-udev
+
+tst sudo cp usr/local/bin/simple-agent.autotrust /usr/local/bin
+tst sudo chmod 755 /usr/local/bin/simple-agent.autotrust
+
+tst sudo cp usr/local/bin/say.sh /usr/local/bin
+tst sudo chmod 755 /usr/local/bin/say.sh
+
+tst sudo cp usr/local/bin/bluezutils.py /usr/local/bin
+
+tst sudo cp etc/pulse/daemon.conf /etc/pulse
+
+sudo patch /boot/config.txt << EOT
+@@ -54,3 +54,6 @@
+ 
+ # Enable audio (loads snd_bcm2835)
+ dtparam=audio=on
++
++# High Quality audio patch
++audio_pwm_mode=2
+EOT
+
+if [ -f /etc/udev/rules.d/99-com.rules ]; then
+
+sudo patch /etc/udev/rules.d/99-com.rules << EOT
+***************
+*** 1 ****
+--- 1,2 ----
+  SUBSYSTEM=="input", GROUP="input", MODE="0660"
++ KERNEL=="input[0-9]*", RUN+="/usr/local/bin/bluez-udev"
+EOT
+
+else
+
+tst sudo touch /etc/udev/rules.d/99-com.rules
+tst sudo chmod 666 /etc/udev/rules.d/99-com.rules
+sudo cat  << EOT > /etc/udev/rules.d/99-input.rules
+SUBSYSTEM=="input", GROUP="input", MODE="0660"
+KERNEL=="input[0-9]*", RUN+="/usr/local/bin/bluez-udev"
+EOT
+
+fi
+
+tst sudo chmod 644 /etc/udev/rules.d/99-com.rules
+
+sudo patch /etc/bluetooth/main.conf << EOT
+***************
+*** 7,8 ****
+--- 7,9 ----
+  #Name = %h-%d
++ Name = ${BT_NAME}
+
+***************
+*** 11,12 ****
+--- 12,14 ----
+  #Class = 0x000100
++ Class = 0x200414
+
+***************
+*** 15,17 ****
+  # 0 = disable timer, i.e. stay discoverable forever
+! #DiscoverableTimeout = 0
+
+--- 17,19 ----
+  # 0 = disable timer, i.e. stay discoverable forever
+! DiscoverableTimeout = 0
+EOT
+
+sudo patch /etc/pulse/system.pa << EOT
+***************
+*** 23,25 ****
+  .ifexists module-udev-detect.so
+! load-module module-udev-detect
+  .else
+--- 23,26 ----
+  .ifexists module-udev-detect.so
+! #load-module module-udev-detect
+! load-module module-udev-detect tsched=0
+  .else
+***************
+*** 57 ****
+--- 58,63 ----
+  load-module module-position-event-sounds
++
++ ### Automatically load driver modules for Bluetooth hardware
++ .ifexists module-bluetooth-discover.so
++     load-module module-bluetooth-discover
++ .endif
+EOT
+
+
+#sudo service bluetooth start &
+#sudo service pulseaudio start &
+#sudo service bluetooth-agent start &
+# BT FIX
+mkdir /etc/pulsebackup
+cp /etc/pulse/* /etc/pulsebackup/
+git clone --branch v6.0 https://github.com/pulseaudio/pulseaudio
+apt-get install intltool
+apt-get install libsndfile-dev
+apt-get install libcap-dev
+apt-get install libjson0-dev
+cd pulseaudio
+./bootstrap.sh
+make
+make install
+ldconfig
+cp /etc/pulsebackup/* /etc/pulse
+
+sleep 5
+
+echo "Done! You should reboot now"
---
+++ install.sh
@@ -0,0 +1,235 @@
+#!/bin/bash
+# Prompt User for Installation
+# Sets Log File
+log="./install.log"
+# Begins Logging
+echo "" > $log
+echo "1. Install the Raspberry Pi Audio Receiver Car Installation"
+echo "2. Install the Raspberry Pi Audio Receiver Home Installation"
+echo "3. Install the Raspberry Pi Network Without Internet Installation (For teaching!)"
+echo "4. Install the Volumio (Bluetooth Only) Installation"
+echo "5. Install a Custom Raspberry Pi Audio Receiver"
+Install="0"
+while [ $Install != "1" ] && [ $Install != "2" ] && [ $Install != "3" ] && [ $Install != "4" ] && [ $Install != "5" ];
+do
+read -p "Which installation would you like to choose? (1/2/3/4/5) : " Install
+# Car Installation - Previously Raspberry Pi Audio Receiver Install Car Install
+if [ $Install = "1" ]
+then
+	AirPlay="y"
+	Bluetooth="y"
+	AP="y"
+	Kodi="y"
+	Lirc="y"
+	SoundCardInstall="y"
+	GMedia="n"
+# Home Installation - Previously Raspberry Pi Audio Receiver Install
+elif [ $Install = "2" ]
+then
+        AirPlay="y"
+        Bluetooth="y"
+        AP="n"
+        Kodi="y"
+        Lirc="y"
+	SoundCardInstall="y"
+	GMedia="y"
+# Access Point Install - Previously Network Without Internet
+elif [ $Install = "3" ]
+then
+	AirPlay="n"
+        Bluetooth="n"
+        AP="y"
+        Kodi="n"
+        Lirc="n"
+	GMedia="n"
+	
+elif [ $Install = "4" ]
+then
+	AirPlay="n"
+	Bluetooth="y"
+	AP="n"
+	Kodi="n"
+	Lirc="n"
+	GMedia="n"
+	SoundCardInstall="n"
+	
+# Custom Install - Allows Users to Choose Installation of various features. Further allowing the use of this project with other ideas aside from Audio Receivers.
+elif [ $Install = "5" ]
+then
+	# Prompts the User to use AirPlay for Streaming (aka shairport-sync)
+	AirPlay="AirPlay"
+	while [ $AirPlay != "y" ] && [ $AirPlay != "n" ];
+	do
+		read -p "Do you want AirPlay Enabled? (y/n) : " AirPlay
+	done
+	# Prompts the User to use Bluetooth for Streaming (aka A2DP)
+	Bluetooth="Bluetooth"
+	while [ $Bluetooth != "y" ] && [ $Bluetooth != "n" ];
+	do
+		read -p "Do you want Bluetooth A2DP Enabled? (y/n) : " Bluetooth
+
+	done
+	# Prompts the User to use the Raspberry Pi as an Access Point to create a network (needed for AirPlay when no existing network exists)
+	AP="AP"
+	while [ $AP != "y" ] && [ $AP != "n" ];
+	do
+		read -p "Do you want to setup as an Access Point? (Necessary for AirPlay, in location without a Wireless Network) (y/n) : " AP
+	done
+	# Prompts the User to use Kodi as a GUI for a Media Center
+	Kodi="Kodi"
+	while [ $Kodi != "y" ] && [ $Kodi != "n" ];
+	do
+		read -p "Do you want Kodi installed? (y/n) : " Kodi
+	done
+	# Prompts the User to use Lirc for Infrared Remote Support (matricom IR remote already setup for use with Kodi)
+	Lirc="Lirc"
+	while [ $Lirc != "y" ] && [ $Lirc != "n" ];
+	do
+		read -p "Do you want to use infrared remotes? (y/n) : " Lirc
+	done
+	SoundCardInstall="SoundCardInstall"
+	while [ $SoundCardInstall != "y" ] && [ $SoundCardInstall != "n" ];
+	do
+		read -p "Do you want to use a Sound Card? (y/n) : " SoundCardInstall
+	done
+	GMedia="GMedia"
+	while [ $GMedia != "y" ] && [ $GMedia != "n" ];
+	do
+		read -p "Do you want to setup device as a UPnP Renderer? (y/n) : " GMedia
+	done
+else
+	echo "Please choose a valid choice"
+fi
+done
+# Prompts the User to check whether or not to use individual names for the chosen devices
+SameName="SameName"
+while [ $SameName != "y" ] && [ $SameName != "n" ];
+do
+	read -p "Do you want all the Devices to use the same name? (y/n) : " SameName
+done
+if [ $SameName = "y" ]
+then
+	# Asks for All Devices Identical Name
+	read -p "Device name: " MYNAME
+	APName=$MYNAME
+	BluetoothName=$MYNAME
+	AirPlayName=$MYNAME
+	GMediaName=$MYNAME
+elif [ $SameName = "n" ]
+then	
+	# Asks for Bluetooth Device Name
+	if [ $Bluetooth = "y" ]
+	then
+		read -p "Bluetooth Device Name: " BluetoothName
+	fi
+	# Asks for AirPlay Device Name
+	if [ $AirPlay = "y" ]
+	then
+		read -p "AirPlay Device Name: " AirPlayName
+	fi
+	# Asks for Access Point Device Name
+	if [ $AP = "y" ]
+	then
+		read -p "Access Point Device Name: " APName
+	fi
+	if [ $GMedia = "y" ]
+	then
+		read -p "UPnP Device Name: " GMediaName	
+	fi
+fi
+if [ $AP = "y" ]
+then
+# Asks for the Access Point Password
+read -p "Device WiFi Password: " WIFIPASS
+fi
+if [ $SoundCardInstall = "y" ]
+then
+	echo "0. No Sound Card"
+	echo "1. HifiBerry DAC Light"
+	echo "2. HifiBerry DAC Standard/Pro"
+	echo "3. HifiBerry Digi+"
+	echo "4. Hifiberry Amp+"
+	echo "5. Pi-IQaudIO DAC"
+	echo "6. Pi-IQaudIO DAC+, Pi-IQaudIO DACZero, Pi-IQaudIO DAC PRO"
+	echo "7. Pi-IQaudIO DigiAMP"
+	echo "8. Pi-IQaudIO Digi+"
+	echo "9. USB Sound Card"
+	echo "10. JustBoom DAC and AMP Cards"
+	echo "11. JustBoom Digi Cards"
+	SoundCard="SoundCard"
+	while [ $SoundCard != "0" ] && [ $SoundCard != "1" ] && [ $SoundCard != "2" ] && [ $SoundCard != "3" ] && [ $SoundCard != "4" ] && [ $SoundCard != "5" ] && [ $SoundCard != "6" ] && [ $SoundCard != "7" ] && [ $SoundCard != "8" ] && [ $SoundCard != "9" ] && [ $SoundCard != "10" ] && [ $SoundCard != "11" ];
+	do
+		read -p "Which Sound Card are you using? (0/1/2/3/4/5/6/7/8/9/10/11) : " SoundCard
+	done
+else
+	SoundCard="0"
+fi
+
+#--------------------------------------------------------------------
+function tst {
+    echo "===> Executing: $*"
+    if ! $*; then
+        echo "Exiting script due to error from: $*"
+        exit 1
+    fi
+}
+#--------------------------------------------------------------------
+chmod +x ./*
+echo "Starting @ `date`" | tee -a $log
+# Updates and Upgrades the Raspberry Pi
+echo "--------------------------------------------" | tee -a $log
+tst ./bt_pa_prep.sh | tee -a $log
+echo "--------------------------------------------" | tee -a $log
+# If Bluetooth is Chosen, it installs Bluetooth Dependencies and issues commands for proper configuration
+if [ $Bluetooth = "y" ]
+then
+	tst ./bt_pa_install.sh | tee -a $log
+	echo "--------------------------------------------" | tee -a $log
+	echo "${BluetoothName}" | tst ./bt_pa_config.sh | tee -a $log
+	echo "--------------------------------------------" | tee -a $log
+fi
+if [ $SoundCardInstall = "y" ]
+then
+	echo "${SoundCard}" | tst ./sound_card_install.sh | tee -a $log
+	echo "--------------------------------------------" | tee -a $log
+fi
+# If AirPlay is Chosen, it installs AirPlay Dependencies and issues commands for proper configuration
+if [ $AirPlay = "y" ]
+then
+	tst ./airplay_install.sh | tee -a $log
+	echo "--------------------------------------------" | tee -a $log
+	{ echo "${AirPlayName}"; echo "${SoundCard}";} | tst ./airplay_config.sh | tee -a $log
+	echo "--------------------------------------------" | tee -a $log
+fi
+# If Access Point is Chosen, it installs AP Dependencies and issues commands for proper configuration
+if [ $AP = "y" ]
+then
+	tst ./ap_install.sh | tee -a $log
+	echo "--------------------------------------------" | tee -a $log
+	{ echo "${APName}"; echo "${WIFIPASS}";} | tst ./ap_config.sh | tee -a $log
+	echo "--------------------------------------------" | tee -a $log
+fi
+# If Kodi is Chosen, it installs Kodi Dependencies and issues commands for proper configuration
+if [ $Kodi = "y" ]
+then
+	tst ./kodi_install.sh | tee -a $log
+	echo "--------------------------------------------" | tee -a $log
+	tst ./kodi_config.sh | tee -a $log
+	echo "--------------------------------------------" | tee -a $log
+fi
+# If Lirc is Chosen, it installs Lirc Dependencies and issues commands for proper configuration
+if [ $Lirc = "y" ]
+then
+	tst ./lirc_install.sh | tee -a $log
+	echo "--------------------------------------------" | tee -a $log
+	tst ./lirc_config.sh | tee -a $log
+	echo "--------------------------------------------" | tee -a $log
+fi
+# If GMedia is Chosen, it installs  GMedia Dependencies and issues commands for proper configuration
+if [ $GMedia = "y" ]
+then
+	echo "${GMediaName}" | tst ./gmrender_install.sh | tee -a $log
+	echo "--------------------------------------------" | tee -a $log
+fi
+echo "Ending at @ `date`" | tee -a $log
+reboot
